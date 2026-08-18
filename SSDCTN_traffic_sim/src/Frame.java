ca14c03eeacfcc4456280d8ca2fe40ebb795901e
Oli <oli.walls@students.mq.edu.au>
2026-08-18 12:48:56 +1000

    Added Frame

--- SSDCTN_traffic_sim/src/Frame.java
+++ SSDCTN_traffic_sim/src/Frame.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import javax.swing.JComponent;
 
-public class Frame{
 
+public class Frame{
     public static void main(String[] args){
         JFrame frame = new JFrame("World Frame");
         frame.setSize(800,800);
