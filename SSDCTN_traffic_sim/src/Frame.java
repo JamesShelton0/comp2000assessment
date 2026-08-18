f55e203afa80ddfbeda4b30401e48e9cf8e8b9a3
Oli <oli.walls@students.mq.edu.au>
2026-08-18 14:25:36 +1000

    Updated notes

--- SSDCTN_traffic_sim/src/Frame.java
+++ SSDCTN_traffic_sim/src/Frame.java
@@ -3,17 +3,15 @@ import java.awt.*;
 import javax.swing.JComponent;
 
 
-public class Frame{
+public class Frame{ // This creates the frame and colours it green 
     public static void main(String[] args){
-        JFrame frame = new JFrame("World Frame");
-        frame.setSize(800,800);
-        frame.setVisible(true);
-        JPanel p = new JPanel();
-        frame.add(p);
-        p.setBackground(Color.green);
-        frame.show();
-
-
+        JFrame frame = new JFrame("World Frame"); // creates a new Frame
+        frame.setSize(800,800); // control the size of the frame
+        frame.setVisible(true); // must be set to true
+        JPanel p = new JPanel(); // creates a new panel object
+        frame.add(p); // connects the frame to the panel
+        p.setBackground(Color.green); // sets background colour - lowercase and american
+        frame.show(); // without this the green does not initalise, idk why the show is crossed out 
     }
 
 
