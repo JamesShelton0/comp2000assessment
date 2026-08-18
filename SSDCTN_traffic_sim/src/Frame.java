175b714c1691ad59ea40b1cfe5ba571607f8e0e9
Oli <oli.walls@students.mq.edu.au>
2026-08-18 14:22:36 +1000

    GREEN BACKGROUND WORKING

--- SSDCTN_traffic_sim/src/Frame.java
+++ SSDCTN_traffic_sim/src/Frame.java
@@ -7,10 +7,17 @@ public class Frame{
     public static void main(String[] args){
         JFrame frame = new JFrame("World Frame");
         frame.setSize(800,800);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-
         frame.setVisible(true);
+        JPanel p = new JPanel();
+        frame.add(p);
+        p.setBackground(Color.green);
+        frame.show();
+
+
     }
+
+
+
 }
 
 
