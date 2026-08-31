31b372e50573ccfc898ad6b4dfa08deb780302da
reii-na <reiiina@pm.me>
2026-08-31 19:31:35 +1000

    drew traffic light + motorbike (lowkey vespa)

--- SSDCTN_traffic_sim/src/Frame.java
+++ SSDCTN_traffic_sim/src/Frame.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-
-
 public class Frame { 
     public static void main(String[] args){
         JFrame frame = new JFrame("World Frame"); 
@@ -11,24 +9,46 @@ public class Frame {
         bground.setBackground(new Color(63, 155, 11));
         bground.setLayout(null);
 
+        // pothole
         Pothole pothole = new Pothole(800, 800);
         pothole.setBounds(0, 0, 800, 800);
         pothole.setOpaque(false);
 
+        // truck
         Truck truck = new Truck(800, 50);
         truck.setBounds(100, 100, 800, 800);
         truck.setOpaque(false);
 
+        // motorbike
+        Motorbike motorbike = new Motorbike();
+        motorbike.setBounds(350,500,50,80);
+        motorbike.setOpaque(false);
+
+        // traffic light
+        TrafficLight trafficLight = new TrafficLight();
+        trafficLight.setBounds(600,100,50,);
+        
+        // adding components 
         bground.add(pothole);
         bground.add(truck);
+        bground.add(motorbike);
+        bground.add(trafficLight);
 
+        // explosion
         Explosion explosion = new Explosion(300, 300);
-        bground.add(explosion); // test explosion !!!!! remove this to not show explosion :(
+        bground.add(explosion);
 
         frame.add(bground);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
+        // change traffic light every X seconds
+        Timer timer = new Timer(4000, e -> {
+            trafficLight.changeLight();
+        });
+
+        timer.start();
     }
 
 
---
+++ SSDCTN_traffic_sim/src/TrafficLight.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TrafficLight extends JComponent {
+    // 0 = green, 1 = orange, 2 = red
+    private int currentLight = 0;
+
+    public TrafficLight() {
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        // shadow 
+        g2d.setColor(new Color(80,80,80,80));
+        g2d.fillOval(10,75,30,7);
+
+        // pole 
+        g2d.setColor(Color.DARK_GRAY);
+        g2d.fillRect(23,52,4,25);
+
+        // traffic light housing
+        g2d.setColor(Color.DARK_GRAY);
+        g2d.fillRoundRect(10,5,30,55,6,6);
+
+        // green light ^-^
+        if (currentLight == 0) {
+            g2d.setColor(Color.GREEN);
+        } else {
+            g2d.setColor(Color.GRAY);
+        }
+        g2d.fillOval(17,9,16,16);
+
+        // orange light o-o
+        if (currentLight == 1) {
+            g2d.setColor(Color.ORANGE);
+        } else {
+            g2d.setColor(Color.GRAY);
+        }
+        g2d.fillOval(17,25,16,16);
+
+        // red light -_-
+        if (currentLight == 2) {
+            g2d.setColor(Color.RED);
+        } else {
+            g2d.setColor(Color.GRAY);
+        }
+        g2d.fillOval(17,41,16,16);
+    }
+
+    public void changeLight() {
+        currentLight++;
+
+        if (currentLight > 2) {
+            currentLight = 0;
+        }
+
+        repaint();
+    }
+}
