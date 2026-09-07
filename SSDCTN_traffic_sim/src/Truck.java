49b3c8dab7aaf571869639d67c49efe87afd584a
Oli <oli.walls@students.mq.edu.au>
2026-09-07 11:43:14 +1000

    Updated Truck Class to Rotate when spawned
    used the same logic as car rotate, faily simple

--- SSDCTN_traffic_sim/src/Truck.java
+++ SSDCTN_traffic_sim/src/Truck.java
@@ -2,92 +2,98 @@ import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
 public class Truck extends Vehicle {
+
     int colour;
     int size;
     int shape;              
     boolean hasSunroof;
+
     Point p;
     HitBox h;
 
     public Truck(double w, double h) {
         super.width = w;
         super.height = h;
+
         p = new Point(500, 500);
         this.h = new HitBox(p, 0, w, h);
-        
-        // super.accelerationRate =
-        // super.decelerationRate =
     }
-
-    // is this method for anything? I removed it but it has re-appeared? Might just ber merge artifact
-    // protected void paintComponent(Graphics g){
-    //     Graphics2D g2d = (Graphics2D) g;
-    //     super.velocity = 0;
-    //     super.direction = 0;
-    //     super.accelerationRate = 0;
-    //     super.decelerationRate = 0;
-    // }
 
     @Override
     public void draw(Graphics2D g2d) {
+
+        Graphics2D g = (Graphics2D) g2d.create();
+
+        g.rotate(
+            Math.toRadians(direction - 180),
+            x,
+            y
+        );
+
+        double left = x - width / 2;
+        double top = y - height / 2;
+
         // Truck cabin
         Rectangle2D.Double truckCabin = new Rectangle2D.Double(
-            x,
-            y,
+            left,
+            y - (height / 5 * 2) / 2,
             width / 7 * 2,
             height / 5 * 2
         );
-        g2d.setColor(Color.RED);
-        g2d.fill(truckCabin);
+
+        g.setColor(Color.RED);
+        g.fill(truckCabin);
 
         // Truck load
         Rectangle2D.Double truckLoad = new Rectangle2D.Double(
-            x + 20,
-            y - 5,
+            left + width / 7 * 2,
+            y - (height / 5 * 3) / 2,
             width / 7 * 5,
             height / 5 * 3
         );
-        g2d.setColor(Color.BLUE);
-        g2d.fill(truckLoad);
+
+        g.setColor(Color.BLUE);
+        g.fill(truckLoad);
 
         // Front left wheel
         Rectangle2D.Double frontLeftWheel = new Rectangle2D.Double(
-            x + 5,
-            y - 2,
+            left + 5,
+            y - (height / 5 * 2) / 2 - 2,
             10,
             4
         );
 
         // Front right wheel
         Rectangle2D.Double frontRightWheel = new Rectangle2D.Double(
-            x + 5,
-            y + height / 5 * 2 - 2,
+            left + 5,
+            y + (height / 5 * 2) / 2 - 2,
             10,
             4
         );
 
         // Rear left wheel
         Rectangle2D.Double rearLeftWheel = new Rectangle2D.Double(
-            x + width - 15,
-            y - 7,
+            left + width - 15,
+            y - (height / 5 * 3) / 2 - 2,
             10,
             4
         );
 
         // Rear right wheel
         Rectangle2D.Double rearRightWheel = new Rectangle2D.Double(
-            x + width - 15,
-            y + height / 5 * 3 - 7,
+            left + width - 15,
+            y + (height / 5 * 3) / 2 - 2,
             10,
             4
         );
 
         // Fill wheels
-        g2d.setColor(Color.BLACK);
-        g2d.fill(frontLeftWheel);
-        g2d.fill(frontRightWheel);
-        g2d.fill(rearLeftWheel);
-        g2d.fill(rearRightWheel);
-    }
+        g.setColor(Color.BLACK);
 
+        g.fill(frontLeftWheel);
+        g.fill(frontRightWheel);
+        g.fill(rearLeftWheel);
+        g.fill(rearRightWheel);
+        g.dispose();
+    }
 }
