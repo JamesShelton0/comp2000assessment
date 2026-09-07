b3eecb5c88172184b650e99fd6913fd68411f6f9
Oli <oli.walls@students.mq.edu.au>
2026-09-07 15:19:34 +1000

    Added function to motorbike

--- SSDCTN_traffic_sim/src/Motorbike.java
+++ SSDCTN_traffic_sim/src/Motorbike.java
@@ -11,23 +11,41 @@ public class Motorbike extends Vehicle {
     }
 
     @Override
-    public void draw(Graphics2D g2d) {
+    public void draw(Graphics2D g2d) { 
+
+        Graphics2D g = (Graphics2D) g2d.create();// same with this, I need a g2d copy to rotate the motorbike without breaking the sim
+
+        g.rotate(
+            Math.toRadians(direction + 90),
+            x,
+            y
+        );     
+
+
+        // draw the motorbike
+        //g.setColor(Color.BLUE);
+        //g.fillRoundRect((int) x, (int) y, (int) width, (int) height, 10, 10);
+
+        // what was the above used for ?? When rotated it makes a big blue square,
+
         // wheels
-        g2d.setColor(Color.BLACK);
-        g2d.fillOval((int) x+15, (int) y+5,20,20);
-        g2d.fillOval((int) x+15, (int) y+55,20,20);
+        g.setColor(Color.BLACK);
+        g.fillOval((int) x+15, (int) y+5,20,20);
+        g.fillOval((int) x+15, (int) y+55,20,20);
 
         // body
-        g2d.setColor(Color.RED);
-        g2d.fillRoundRect((int) x+12, (int) y+22,26,40,8,8);
+        g.setColor(Color.RED);
+        g.fillRoundRect((int) x+12, (int) y+22,26,40,8,8);
 
         // seat
-        g2d.setColor(Color.BLACK);
-        g2d.fillRoundRect((int) x+16, (int) y+35,18, 15,5,5);
+        g.setColor(Color.BLACK);
+        g.fillRoundRect((int) x+16, (int) y+35,18, 15,5,5);
 
         // handlebar
-        g2d.setColor(Color.DARK_GRAY);
-        g2d.fillRect((int) x+10, (int) y+20,30,3);
+        g.setColor(Color.DARK_GRAY);
+        g.fillRect((int) x+10, (int) y+20,30,3);
+
+        g.dispose();
 
     }
 }
